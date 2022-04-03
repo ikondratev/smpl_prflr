93725dfbe34a9358a9a6cb19de95e9c1a6790e02
kilya <ilya.kondratyev@kupibilet.ru>
2022-04-03 17:19:53 +0200

    [0007] Add license

--- config/application.rb
+++ config/application.rb
@@ -5,5 +5,11 @@ require 'bundler'
 
 Bundler.require(:default, ENV["RACK_ENV"] || "development")
 
+# require additional gem files
+require "active_support/all"
+
+# initialize application, logger, gems, etc.
+require_all "config/initializers"
+
 # require application
 require_all "app"
